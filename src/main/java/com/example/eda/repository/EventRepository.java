6cb6f3857c35aad3afe07ab1c282ac4ab97bbbf5
Wallan <wallanaq.dev@gmail.com>
2025-03-11 11:01:44 -0300

    add retry with dlt

--- src/main/java/com/example/eda/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eda.repository;
-
-import com.example.eda.entity.EventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends JpaRepository<EventEntity, String> {
-}
--- src/main/java/com/example/eda/service/KafkaConsumerService.java
+++ src/main/java/com/example/eda/service/KafkaConsumerService.java
@@ -1,14 +1,14 @@
 package com.example.eda.service;
 
-import com.example.eda.entity.EventEntity;
-import com.example.eda.mapper.EventMapper;
-import com.example.eda.repository.EventRepository;
 import com.example.eda.schemas.EventSchema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,35 +16,43 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KafkaConsumerService {
 
-  private final EventMapper mapper;
-  private final EventRepository repository;
   private final KafkaTemplate<String, EventSchema> kafkaTemplate;
 
-  private static final String TOPIC_DLT = "event-stream-dlt";
-
-  @KafkaListener(topics = "event-stream", groupId = "event-consumers")
+  @RetryableTopic(
+    attempts = "3",
+    dltTopicSuffix = "-dlt",
+    backoff = @Backoff(delay = 3000)
+  )
+  @KafkaListener(
+    topics = "event-stream",
+    groupId = "event-consumers"
+  )
   public void consume(ConsumerRecord<String, EventSchema> record) {
     EventSchema event = record.value();
-
-    EventEntity entity = mapper.toEntity(event);
 
     var correlationId = new String(record.headers().lastHeader("correlationId").value());
 
     try {
 
       if (event.getName().toString().contains("ERROR")) {
-          log.error("Database error [eventId: {}][correlationId {}]", event.getEventId().toString(), correlationId);
-        throw new RuntimeException("Simulated database error");
+        log.error("Event error [eventId: {}][correlationId {}]", event.getEventId().toString(), correlationId);
+        throw new RuntimeException("Simulated error");
       }
 
-      this.repository.save(entity);
       log.info("Event saved successfully [eventId: {}][correlationId {}]", event.getEventId().toString(), correlationId);
 
     } catch (Exception e) {
 
       log.error("Failed to save event[eventId: {}][correlationId {}]", event.getEventId().toString(), correlationId);
-      kafkaTemplate.send(TOPIC_DLT, event.getEventId().toString(), event);
+      throw e;
 
     }
+  }
+
+  @DltHandler
+  public void handleDltMessage(ConsumerRecord<String, EventSchema> record) {
+    EventSchema event = record.value();
+    var correlationId = new String(record.headers().lastHeader("correlationId").value());
+    log.error("DLT received failed message [eventId: {}][correlationId {}]", event.getEventId().toString(), correlationId);
   }
 }
